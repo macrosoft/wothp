ca61c947aca8ff63bc97483d2390c106b16ec96d
Zagidulin Artyom <p0chta@yandex.ru>
2014-03-25 22:05:15 +0400

    paint text hello world

--- src/totalhp.py
+++ src/totalhp.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import BigWorld
+import GUI
 from gui.Scaleform.Battle import Battle
 from debug_utils import *
 
@@ -8,6 +9,21 @@ old_Battle_afterCreate = Battle.afterCreate
 
 def new_Battle_afterCreate(self):
     old_Battle_afterCreate(self)
-    LOG_NOTE("hello world!")
+    window = GUI.Window('')
+    sr = GUI.screenResolution()
+    window = GUI.Window('')
+    window.heightMode = 'PIXEL'
+    window.widthMode = 'PIXEL'
+    window.width = sr[0]
+    window.height = sr[1]
+    GUI.addRoot(window)
+    text = GUI.Text("12345 / 67890")
+    text.font = 'default_smaller.font'
+    text.colour = (255.0, 255.0, 255.0, 255.0)
+    text.materialFX = 'ADD'
+    window.addChild(text)
+    text.horizontalPositionMode = 'PIXEL'
+    text.verticalPositionMode = 'PIXEL'
+    text.position = (sr[0]/2, 45, 1)
 
 Battle.afterCreate = new_Battle_afterCreate
