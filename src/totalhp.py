0fbae3f6f8b87dbe538d0aae78b7a51245ce8e36
Artyom Zagidulin <p0chta@yandex.ru>
2014-03-26 13:31:45 +0400

    remove label after battle

--- src/totalhp.py
+++ src/totalhp.py
@@ -5,25 +5,53 @@ import GUI
 from gui.Scaleform.Battle import Battle
 from debug_utils import *
 
+class Wothp(object):
+    obj = None
+
+    def __init__(self):
+        self.window = None
+        self.label = None
+
+    def __new__(self, *dt, **mp):
+        if self.obj is None:
+            self.obj = object.__new__(self, *dt, **mp)
+        return self.obj
+
+    def createLabel(self):
+        self.window = GUI.Window('')
+        sr = GUI.screenResolution()
+        self.window.heightMode = 'PIXEL'
+        self.window.widthMode = 'PIXEL'
+        self.window.width = sr[0]
+        self.window.height = sr[1]
+        GUI.addRoot(self.window)
+        self.label = GUI.Text("12345 / 67890")
+        self.label.font = 'Courier New_15.dds'
+        self.label.colour = (255.0, 255.0, 255.0, 255.0)
+        self.label.materialFX = 'ADD'
+        self.window.addChild(self.label)
+        self.label.horizontalPositionMode = 'PIXEL'
+        self.label.verticalPositionMode = 'PIXEL'
+        self.label.position = (sr[0]/2, 45, 1)
+
+    def deleteLabel(self):
+        GUI.delRoot(self.window)
+        self.window = None
+
 old_Battle_afterCreate = Battle.afterCreate
 
 def new_Battle_afterCreate(self):
     old_Battle_afterCreate(self)
-    window = GUI.Window('')
-    sr = GUI.screenResolution()
-    window = GUI.Window('')
-    window.heightMode = 'PIXEL'
-    window.widthMode = 'PIXEL'
-    window.width = sr[0]
-    window.height = sr[1]
-    GUI.addRoot(window)
-    text = GUI.Text("12345 / 67890")
-    text.font = 'default_smaller.font'
-    text.colour = (255.0, 255.0, 255.0, 255.0)
-    text.materialFX = 'ADD'
-    window.addChild(text)
-    text.horizontalPositionMode = 'PIXEL'
-    text.verticalPositionMode = 'PIXEL'
-    text.position = (sr[0]/2, 45, 1)
+    wothp = Wothp()
+    wothp.createLabel()
 
 Battle.afterCreate = new_Battle_afterCreate
+
+old_Battle_beforeDelete = Battle.beforeDelete
+
+def new_beforeDelete(self):
+    old_Battle_beforeDelete(self)
+    wothp = Wothp()
+    wothp.deleteLabel()
+
+Battle.beforeDelete = new_beforeDelete
