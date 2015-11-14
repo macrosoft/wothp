95dede9c1013fd78dfebf6f02d98d782d09d9df7
Zagidulin Artyom <p0chta@yandex.ru>
2015-11-14 17:46:29 +0300

    port to 0.9.12 Common Test

--- compiler.py
+++ compiler.py
@@ -1,6 +1,6 @@
 import py_compile, zipfile, os
 
-WOTVersion = "0.9.10"
+WOTVersion = "0.9.12 Common Test"
 language = 'ru' # 'ru' or 'en'
 
 if os.path.exists("totalhp.zip"):
@@ -16,4 +16,8 @@ fZip.write("src/totalhp.pyc", WOTVersion+"/scripts/client/mods/totalhp.pyc")
 fZip.write("src/CameraNode.pyc", WOTVersion+"/scripts/client/CameraNode.pyc")
 fZip.write("data/totalhp_"+language+".json", WOTVersion+"/scripts/client/mods/totalhp.json")
 fZip.write("data/totalhp_bg.dds", WOTVersion+"/scripts/client/mods/totalhp_bg.dds")
+fZip.write("data/default_medium.font", WOTVersion+"/system/fonts/default_medium.font")
+fZip.write("data/default_smaller.font", WOTVersion+"/system/fonts/default_smaller.font")
+fZip.write("data/HGSoeiKakugothicUB_12.dds", WOTVersion+"/system/fonts/HGSoeiKakugothicUB_12.dds")
+fZip.write("data/HGSoeiKakugothicUB_20.dds", WOTVersion+"/system/fonts/HGSoeiKakugothicUB_20.dds")
 fZip.close()
